02a94f750f9927eb15fab612c47d3e01deb991ef
ajaykanniyappan <ajaykanniyappan@gmail.com>
2023-01-27 17:56:13 +0530

    Tailwind & vscode configurations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import styles from "@/styles/Home.module.css";
-
 export default function Home() {
   return (
     <>
-      <h1 className={styles.center}>Hello World!</h1>
+      <h1 className="text-3xl font-bold text-center">Hello world!</h1>
     </>
   );
 }
