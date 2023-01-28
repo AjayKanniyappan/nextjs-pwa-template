a6c2865e48043d35964317799e23351658857564
ajaykanniyappan <ajaykanniyappan@gmail.com>
2023-01-28 10:54:29 +0530

    Eslint & Prettier configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold text-center">Hello world!</h1>
-    </>
-  );
+  return <h1 className="text-3xl font-bold text-center">Hello world!</h1>;
 }
