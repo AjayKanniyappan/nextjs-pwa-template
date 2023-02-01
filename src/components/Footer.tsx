706540199b3201768c9efa11817b628bb70a7085
ajaykanniyappan <ajaykanniyappan@gmail.com>
2023-02-01 23:02:20 +0530

    NavBar added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="hidden md:block">
+    <div className="hidden sm:block">
       <footer className="fixed bottom-0 w-full px-4 divide-y dark:bg-gray-800 dark:text-gray-100">
         <div className="py-6 text-md text-center dark:text-gray-400">
           <p className="text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,35 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const navBar: Array<{ id: number; label: string; href: string }> = [
+  { id: 1, label: 'Facts', href: '/facts' },
+  { id: 2, label: 'Breeds', href: '/breeds' },
+];
+
 function NavBar(): JSX.Element {
+  const router = useRouter();
+
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
+    <div className="hidden sm:block justify-end">
+      <nav className="text-lg">
+        <ul className="flex items-center">
+          {navBar.map((links) => (
+            <li key={links.id}>
+              <Link
+                href={links.href}
+                className={`${
+                  router.pathname === links.href
+                    ? 'text-indigo-500 dark:text-indigo-400'
+                    : 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
+                }`}
+              >
+                <span className="">{links.label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
